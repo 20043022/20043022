45dc3bd7be18677516f3aa150ca97802568d779c
20043022 <Shivabala603@gmail.com>
2024-12-02 12:53:06 +0000

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const bodyparser = require("body-parser");
-app = express();
-app.use(bodyparser.urlencoded({ extended: false }));
-app.listen(3000);
-app.get("", function (req, res) {
-  res.sendFile(__dirname + "/templets/index.html");
-});
